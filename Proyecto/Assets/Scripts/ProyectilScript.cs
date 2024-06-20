5ff7b53dc7081ab52765a2315c1ba864e67016fa
XimeXim <xbc02@alumnos.ucn.cl>
2024-06-19 20:53:10 -0400

    particulas explosivo

--- Proyecto/Assets/Scripts/ProyectilScript.cs
+++ Proyecto/Assets/Scripts/ProyectilScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ProyectilScript : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public ParticleSystem particulas;
     void Start()
     {
     }
@@ -15,5 +15,19 @@ public class ProyectilScript : MonoBehaviour
     {
 
     }
-    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+     if(particulas!=null)
+        {
+            particulas.Play();
+        }  
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (particulas !=null) {
+            particulas.Play();
+        }
+    }
+
 }
