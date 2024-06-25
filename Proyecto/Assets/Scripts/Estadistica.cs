ece0f703590f92c79153ccd3fc58e46d5fb86bc1
Miguel Araya Cortes <miguel.araya02@alumnos.ucn.cl>
2024-06-25 11:26:18 -0400

    added stats singleton

--- Proyecto/Assets/Scripts/Estadistica.cs
+++ Proyecto/Assets/Scripts/Estadistica.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class Estadistica : MonoBehaviour
 {
+    private static Estadistica _instancia;
+    public int win;
+    public int lose;
+    public int blocks_drop;
+     public static Estadistica Instancia
+    {
+        get
+        {
+            if (_instancia == null)
+            {
+                _instancia = FindObjectOfType<Estadistica>();
+                if (_instancia == null)
+                {
+                    GameObject singletonObject = new GameObject(typeof(Estadistica).ToString());
+                    _instancia = singletonObject.AddComponent<Estadistica>();
+                }
+            }
+            return _instancia;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
