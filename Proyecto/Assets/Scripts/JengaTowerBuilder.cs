b1293bed5b71ca75bd53ab91e01a9b9a12d77d6c
Miguel Araya Cortes <miguel.araya02@alumnos.ucn.cl>
2024-06-25 12:53:36 -0400

    fixed to 5 the layers

--- Proyecto/Assets/Scripts/JengaTowerBuilder.cs
+++ Proyecto/Assets/Scripts/JengaTowerBuilder.cs
@@ -18,22 +18,23 @@ public class JengaTowerBuilder : MonoBehaviour
     {
         // Dimensiones del bloque
         float blockLength = 0.075f; // Largo del bloque
-        float blockHeight = 0.015f; // Alto del bloque
+        //float blockHeight = 0.015f; // Alto del bloque
+        float blockHeight = 0.02f; // Alto del bloque
         float blockWidth = 0.025f; // Ancho del bloque
-
+        float distance = 0f;
         Vector3 startPosition = transform.position;
         for(int i=0;i<layers;i++){
             if(i%2==0){
                 for(int j=0;j<3;j++){
                     Vector3 position;
-                    position = startPosition + new Vector3(0, i * blockHeight, j*blockWidth);
+                    position = startPosition + new Vector3(0, distance+i * blockHeight, j*blockWidth);
                     GameObject block = Instantiate(blockPrefab, position, Quaternion.identity);
                     block.transform.parent = parent.transform;
                 }
             }else{
                 for(int j=0;j<3;j++){
                     Vector3 position;
-                    position = startPosition + new Vector3(j*blockWidth, i * blockHeight,0);
+                    position = startPosition + new Vector3(j*blockWidth, distance+i * blockHeight,0);
                     
                     GameObject block = Instantiate(blockPrefab, position, Quaternion.identity);
                     block.transform.Rotate(0, 90, 0);
